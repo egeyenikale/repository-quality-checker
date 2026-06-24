69a9c59613f6ebedbb869bad22b07aceb401eed9
Ege Hidayet Koca <kocaege777@gmail.com>
2026-06-24 16:49:00 +0300

    docs: add installation and usage documentation

--- src/implementation-roadmap.js
+++ src/implementation-roadmap.js
@@ -28,3 +28,8 @@ export const stage6 = {
   "date": "2026-06-23",
   "message": "test: add automated coverage"
 };
+
+export const stage7 = {
+  "date": "2026-06-24",
+  "message": "docs: add installation and usage documentation"
+};
